dfb73b8a4cf405492f72c9ff64f5152efa51ca82
carlos <carlos01ep@gmail.com>
2023-03-05 07:13:32 +0100

    estilos

--- WpfGameFrenzy/WpfGameFrenzy/TiendaCarlos.xaml.cs
+++ WpfGameFrenzy/WpfGameFrenzy/TiendaCarlos.xaml.cs
@@ -24,5 +24,43 @@ namespace WpfGameFrenzy
         {
             InitializeComponent();
         }
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabControl tabControl = sender as TabControl;
+            if (tabControl != null && tabControl.SelectedItem != null)
+            {
+                TabItem tabItem = tabControl.ItemContainerGenerator.ContainerFromItem(tabControl.SelectedItem) as TabItem;
+                if (tabItem != null)
+                {
+                    tabItem.Background = Brushes.Black;
+                }
+            }
+        }
+
+        private void myButton1_Click(object sender, RoutedEventArgs e)
+        {
+            myImage.Source = new BitmapImage(new Uri("viedeojuego1.jpg", UriKind.Relative));
+        }
+
+        private void myButton2_Click(object sender, RoutedEventArgs e)
+        {
+            myImage.Source = new BitmapImage(new Uri("viedeojuego2.jpg", UriKind.Relative));
+        }
+
+        private void myButton3_Click(object sender, RoutedEventArgs e)
+        {
+            myImage.Source = new BitmapImage(new Uri("viedeojuego3.jpg", UriKind.Relative));
+        }
+
+        private void myButton4_Click(object sender, RoutedEventArgs e)
+        {
+            myImage.Source = new BitmapImage(new Uri("viedeojuego4.jpg", UriKind.Relative));
+        }
+
+        private void myButton5_Click(object sender, RoutedEventArgs e)
+        {
+            myImage.Source = new BitmapImage(new Uri("viedeojuego5.jpg", UriKind.Relative));
+        }
     }
+
 }
